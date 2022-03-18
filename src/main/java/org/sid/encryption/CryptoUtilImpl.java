40513bce2f9f05ea2940f92aea6f2f454f1f73af
Boudriga@DESKTOP-SEENFKV <Boudriga@DESKTOP-SEENFKV>
2022-03-18 10:51:35 +0100

    third commit

--- src/main/java/org/sid/encryption/CryptoUtilImpl.java
+++ src/main/java/org/sid/encryption/CryptoUtilImpl.java
@@ -7,7 +7,13 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Formatter;
 
@@ -61,5 +67,59 @@ public class CryptoUtilImpl {
         cipher.init(Cipher.DECRYPT_MODE,secretKey);
         byte[] decryptedBytes = cipher.doFinal(decodeEncryptedData);
         return decryptedBytes ;
+    }
+
+    public KeyPair generateKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(1024);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    public PublicKey publicKeyFromBase64(String pkBase64) throws Exception {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        byte[] decodePK = Base64.getDecoder().decode(pkBase64);
+        PublicKey publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(decodePK));
+        return publicKey ;
+    }
+
+    public PrivateKey privateKeyFromBase64(String pkBase64) throws Exception {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        byte[] decodePK = Base64.getDecoder().decode(pkBase64);
+        PrivateKey privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(decodePK));
+        return privateKey ;
+    }
+
+    public String encryptRSA(byte[] data, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] bytes = cipher.doFinal(data);
+        return encodeToBase64(bytes);
+    }
+
+    public byte[] decryptRSA(String dataBase64, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decodedEncrypteData = decodeFromBase64(dataBase64);
+        byte[] decryptData = cipher.doFinal(decodedEncrypteData);
+        return decryptData;
+    }
+
+    public PublicKey publicKeyFromCertificate(String fileName) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        Certificate certificate =certificateFactory.generateCertificate(fileInputStream);
+        System.out.println(" ========================================== ");
+        System.out.println(certificate.toString());
+        System.out.println(" ========================================== ");
+        return certificate.getPublicKey();
+    }
+
+    public PrivateKey privateKeyFromJSK(String fileName, String jksPassWord, String alias) throws Exception {
+        FileInputStream fileInputStream= new FileInputStream(fileName);
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(fileInputStream, jksPassWord.toCharArray());
+        Key key =keyStore.getKey(alias, jksPassWord.toCharArray());
+        PrivateKey privateKey = (PrivateKey) key ;
+        return privateKey ;
     }
 }
